bd06f557b62e2b8493ca90a6e321c2fc3279e5a1
Adrielus <yaninoxdev@gmail.com>
2026-06-30 18:07:42 -0400

    Added the stylesheet and js script for the functions.

---
+++ buttons.js
@@ -0,0 +1,8 @@
+function button_a() {
+  alert("Seems this button works!");
+}
+
+function button_b() {
+  var now = "It is currently " + new Date().toLocaleString() + ", and I will become a good developer!"
+  alert(now);
+}
